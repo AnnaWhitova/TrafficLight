b81e92b915466a1ff356cb5da52e6393af7409c9
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-05-09 00:38:30 +0300

    set up button logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    private let switchOn: CGFloat = 1
+    private let switchOff: CGFloat = 0.3
+    
+    private var redLight = true
+    private var yellowLight = false
+    private var greenLight = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        redLightView.alpha = switchOff
+        redLightView.layer.cornerRadius = 50
+       
+        yellowLightView.alpha = switchOff
+        yellowLightView.layer.cornerRadius = 50
+        
+        greenLightView.alpha = switchOff
+        greenLightView.layer.cornerRadius = 50
     }
 
-
+    
+    
+    @IBAction func lightSwitchButtonDidTap(_ sender: UIButton) {
+        
+        sender.setTitle("NEXT", for: .normal)
+        
+        if redLight {
+            redLightView.alpha = switchOn
+            yellowLightView.alpha = switchOff
+            greenLightView.alpha = switchOff
+            yellowLight = true
+            redLight = false
+        } else if yellowLight {
+            redLightView.alpha = switchOff
+            yellowLightView.alpha = switchOn
+            greenLight = true
+            yellowLight = false
+        } else if greenLight {
+            yellowLightView.alpha = switchOff
+            greenLightView.alpha = switchOn
+            greenLight = false
+            redLight = true
+        }
+        
+    }
 }
 
